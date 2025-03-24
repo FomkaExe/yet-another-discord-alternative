1d53bccf5ffa153d0d472ae5b14519a4bbb1ac4a
FomkaExe <fomkaexe@yandex.ru>
2025-03-24 10:57:45 +0300

    Fix server initialisation

--- server/main.cpp
+++ server/main.cpp
@@ -2,14 +2,9 @@
 #include "server.h"
 
 int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
     Server* server = argc <= 1 ?
-                     Server::instance(23012) :
-                     Server::instance(std::stoi(argv[1]));
-
-    while (true) {
-        QTextStream qtin(stdin);
-        QString cmd = qtin.readLine();
-        server->sendToAllClients(cmd);
-    }
-    return 0;
+                         Server::instance(23012) :
+                         Server::instance(std::stoi(argv[1]));
+    return a.exec();
 }
