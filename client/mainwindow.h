b367d1cf886e0746e77b14107769ba7f841fdbe7
FomkaExe <fomkaexe@yandex.ru>
2025-03-24 16:31:34 +0300

    add menu, add actions, add connection prompt

--- client/mainwindow.h
+++ client/mainwindow.h
@@ -7,6 +7,7 @@
 #include <QTextEdit>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QMenuBar>
 
 #include "client.h"
 
@@ -17,18 +18,33 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private:
+    void initConnectWindow();
+
 private slots:
     void slotSendMessage();
     void slotShowMessage(const QString& message);
+    void slotClientConnect(const QString& address);
+    void slotClientDisconnect();
 
 private:
+    /* Widgets */
     QWidget* m_centralWidget;
     QVBoxLayout* m_layout;
     QLabel* m_programNameLabel;
     QTextEdit* m_txtInfo;
     QLineEdit* m_txtInput;
     QPushButton* m_sendButton;
+    QMenuBar* m_menuBar;
+    QMenu* m_connectMenu;
+    QMenu* m_aboutMenu;
+    QAction* m_connectAct;
+    QAction* m_disconnectAct;
+    QAction* m_exitAct;
+    QAction* m_aboutQtAct;
+    QWidget* m_connectWindow;
 
+    /* Client logic */
     Client* m_client;
 };
 #endif // MAINWINDOW_H
