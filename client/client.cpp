d9e7d93c7345b12156a11eaf6e4034fa5f2ec979
FomkaExe <fomkaexe@yandex.ru>
2025-03-24 11:09:37 +0300

    Add client class implementation

---
+++ client/client.cpp
@@ -0,0 +1,51 @@
+#include "client.h"
+
+Client::Client(QObject *parent)
+    : QObject{parent},
+    m_socket(new QTcpSocket()),
+    m_nextBlockSize(0) {
+    connect(m_socket, &QAbstractSocket::errorOccurred,
+            this, &Client::slotErrorOccurred);
+    connect(m_socket, &QIODevice::readyRead,
+            this, &Client::slotReadyRead);
+}
+
+void Client::connectToServer(const QString& ipv4, int port) {
+    m_socket->connectToHost(ipv4, port);
+}
+
+qint64 Client::sendToServer(const QString& message) {
+    QByteArray barr;
+    QDataStream out(&barr, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_5);
+    out << quint16(0) << message;
+    out.device()->seek(0);
+    out << quint16(barr.size() - sizeof(quint16));
+    return m_socket->write(barr);
+}
+
+void Client::slotErrorOccurred(QAbstractSocket::SocketError socketError) {
+    qDebug() << socketError;
+}
+
+void Client::slotReadyRead() {
+    QDataStream in(m_socket);
+    in.setVersion(QDataStream::Qt_6_5);
+    while (true) {
+        if (!m_nextBlockSize) {
+            if (in.device()->bytesAvailable() < sizeof(quint16)) {
+                break;
+            }
+            in >> m_nextBlockSize;
+        }
+
+        if (in.device()->bytesAvailable() < m_nextBlockSize - sizeof(quint16)) {
+            break;
+        }
+        QString str;
+        in >> str;
+        emit readyReadSuccess(str);
+
+        m_nextBlockSize = 0;
+    }
+}
---
+++ client/client.h
@@ -0,0 +1,27 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QObject>
+#include <QTcpSocket>
+
+class Client : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Client(QObject *parent = nullptr);
+    void connectToServer(const QString& ipv4, int port);
+    qint64 sendToServer(const QString& message);
+
+private slots:
+    void slotErrorOccurred(QAbstractSocket::SocketError socketError);
+    void slotReadyRead();
+
+signals:
+    void readyReadSuccess(const QString& message);
+
+private:
+    QTcpSocket* m_socket;
+    quint16 m_nextBlockSize;
+};
+
+#endif // CLIENT_H
