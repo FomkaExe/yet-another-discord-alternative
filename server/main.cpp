87f3b626875409915d79e80ba18fcbfbd11738e3
FomkaExe <fomkaexe@yandex.ru>
2025-04-08 13:19:20 +0300

    Minor refactoring

--- server/main.cpp
+++ server/main.cpp
@@ -6,6 +6,5 @@ int main(int argc, char *argv[]) {
     Server* server = argc <= 1 ?
                          Server::instance(23012) :
                          Server::instance(std::stoi(argv[1]));
-    qDebug() << "Main thread: " << QThread::currentThreadId();
     return a.exec();
 }
